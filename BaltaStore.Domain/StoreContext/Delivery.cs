3805510e906270ffbb745d13f9e44a0c7676f29c
Natanael <--replace-all>
2021-10-05 21:28:11 -0300

    Definir as Entidades

---
+++ BaltaStore.Domain/StoreContext/Delivery.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BaltaStore.Domain.StoreContext
+{
+    public class Delivery
+    {
+        public DateTime CreateDate { get; set; }
+
+        public DateTime EstimatedDeliveryDate { get; set; }
+
+        public string Status { get; set; }
+    }
+}
