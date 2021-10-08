5dd2d09f7027dac78d86b7454ef1dc21365b736e
Natanael <natanaelsantosbr@gmail.com>
2021-10-07 21:12:20 -0300

    Testando VO

---
+++ BaltaStore.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,32 @@
+﻿using BaltaStore.Domain.StoreContext.Entities;
+using BaltaStore.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaltaStore.Tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        private Document _validDocument;
+        private Document _invalidDocument;
+
+        public DocumentTests()
+        {
+            this._invalidDocument = new Document("11132112395");
+            this._validDocument = new Document("03753828106");
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsNotValid()
+        {            
+            Assert.AreEqual(false,  this._invalidDocument.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsValid()
+        {            
+            Assert.AreEqual(true, this._validDocument.IsValid);
+        }
+
+    }
+}
---
+++ BaltaStore.Tests/ValueObjects/NameTests.cs
@@ -0,0 +1,32 @@
+﻿using BaltaStore.Domain.StoreContext.Entities;
+using BaltaStore.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaltaStore.Tests.ValueObjects
+{
+    [TestClass]
+    public class NameTests
+    {
+        private Name _validDocument;
+        private Name _invalidDocument;
+
+        public NameTests()
+        {
+            this._invalidDocument = new Name("", "");
+            this._validDocument = new Name("Natanael", "Santos");
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsNotValid()
+        {
+            Assert.AreEqual(false, this._invalidDocument.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsValid()
+        {
+            Assert.AreEqual(true, this._validDocument.IsValid);
+        }
+
+    }
+}
