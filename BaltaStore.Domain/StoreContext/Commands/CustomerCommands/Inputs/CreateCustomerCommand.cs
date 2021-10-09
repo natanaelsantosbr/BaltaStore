78b54e0e33ad8e37908ff57ffcc01081fe87f92b
Natanael <natanaelsantosbr@gmail.com>
2021-10-08 22:56:32 -0300

    Criar Commands para outras Entidades

---
+++ BaltaStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs
+{
+    public class CreateCustomerCommand
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Document { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
---
+++ BaltaStore.Domain/StoreContext/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
@@ -0,0 +1,26 @@
+﻿using BaltaStore.Domain.StoreContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaltaStore.Domain.StoreContext.Commands.OrderCommands.Inputs
+{
+    public class PlaceOrderCommand
+    {
+        public Guid Customer { get; set; }
+
+        public IList<OrderItemCommand> OrderItems { get; set; }
+
+
+
+    }
+
+    public class OrderItemCommand
+    {
+        public Guid Product { get; set; }
+
+        public decimal Quantity { get; set; }
+    }
+}
