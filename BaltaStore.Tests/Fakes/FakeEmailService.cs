beebef8ecf8b93773d25b9eac684d3137f389551
Natanael <natanaelsantosbr@gmail.com>
2021-10-12 09:47:14 -0300

    Mock e Fakes

---
+++ BaltaStore.Tests/Fakes/FakeEmailService.cs
@@ -0,0 +1,17 @@
+﻿using BaltaStore.Domain.StoreContext.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaltaStore.Tests.Fakes
+{
+    public class FakeEmailService : IEmailService
+    {
+        public void Send(string to, string from, string subject, string body)
+        {
+            
+        }
+    }
+}
---
+++ BaltaStore.Tests/Handlers/CustomerHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using BaltaStore.Domain.Handlers;
+using BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs;
+using BaltaStore.Tests.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaltaStore.Tests.Handlers
+{
+    [TestClass]
+    public class CustomerHandlerTests
+    {
+        [TestMethod]
+        public void ShouldRegisterCustomerWhenCommandIsValid()
+        {
+            var command = new CreateCustomerCommand
+            {
+                FirstName = "Natanael",
+                LastName = "Santos",
+                Document = "02164315065",
+                Email = "natanaelsantosbr@gmail.com",
+                Phone = "61981145200"
+            };
+
+            Assert.AreEqual(true, command.Valid());
+
+            var handler = new CustomerHandler(new FakeCustomerRepository(), new FakeEmailService());
+        }
+
+    }
+}
