1aa710682a587c0ea7e9c06d47354e7a62ce7847
Natanael <natanaelsantosbr@gmail.com>
2021-10-12 11:34:04 -0300

    DataContext

---
+++ BaltaStore.Infra/StoreContext/DataContext/BaltaDataContext.cs
@@ -0,0 +1,28 @@
+﻿using BaltaStore.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaltaStore.Infra.StoreContext.DataContext
+{
+    public class BaltaDataContext : IDisposable
+    {
+        public SqlConnection Connection { get; set; }
+
+        public BaltaDataContext()
+        {
+            this.Connection = new SqlConnection(Settings.ConnectionString);
+            this.Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            if (this.Connection.State != ConnectionState.Closed)
+                this.Connection.Close();
+        }
+    }
+}
