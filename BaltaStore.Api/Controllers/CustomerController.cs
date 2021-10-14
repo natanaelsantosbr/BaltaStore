c542c3a28ea5b642727eca0a913415ddb434b641
Natanael <natanaelsantosbr@gmail.com>
2021-10-13 22:31:56 -0300

    Cache

--- BaltaStore.Api/Controllers/CustomerController.cs
+++ BaltaStore.Api/Controllers/CustomerController.cs
@@ -27,6 +27,7 @@ namespace BaltaStore.Api.Controllers
 
         [HttpGet]
         [Route("v1/customers")]
+        [ResponseCache(Duration = 60)]
         public IEnumerable<ListCustomerQueryResult> Get()
         {
             return this._repository.Get();
